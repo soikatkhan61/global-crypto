d89516070296b2dab025f3039b9c185f78f2315e
Soikat Hossain <soikatkhan61@gmail.com>
2022-08-28 20:59:45 +0600

    make it better

--- controllers/auth.js
+++ controllers/auth.js
@@ -211,17 +211,88 @@ let v_id = Math.floor(Math.random() * (9999 - 1111 + 1) + 1111);
           accessToken: accessToken,
         },
       });
-  
+      let v_idd = v_id.toString().split('').join(' ')
       const mailOptions = {
-        from: "codeDocz <dev.soikathossain@gmail.com>",
+        from: "<dev.soikathossain@gmail.com>",
         to: `${email}`,
-        subject: "Please verify your account!",
+        subject: "Thank You for Sing up! Just do one step more!",
         html: `
-                        <div>
-                            <p>your verification code is:  
-                            </p>
-                            <h1> ${v_id} </h1>
-                        </div>
+        <!doctype html>
+        <html lang="en-US">
+        
+        <head>
+            <meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+            <title>Reset Password Email Template</title>
+            <meta name="description" content="Reset Password Email Template.">
+            <style type="text/css">
+                a:hover {text-decoration: underline !important;}
+            </style>
+        </head>
+        
+        <body marginheight="0" topmargin="0" marginwidth="0" style="margin: 0px; background-color: #f2f3f8;" leftmargin="0">
+            <!--100% body table-->
+            <table cellspacing="0" border="0" cellpadding="0" width="100%" bgcolor="#f2f3f8"
+                style="@import url(https://fonts.googleapis.com/css?family=Rubik:300,400,500,700|Open+Sans:300,400,600,700); font-family: 'Open Sans', sans-serif;">
+                <tr>
+                    <td>
+                        <table style="background-color: #f2f3f8; max-width:670px;  margin:0 auto;" width="100%" border="0"
+                            align="center" cellpadding="0" cellspacing="0">
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                  
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <table width="95%" border="0" align="center" cellpadding="0" cellspacing="0"
+                                        style="max-width:670px;background:#fff; border-radius:3px; text-align:center;-webkit-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);-moz-box-shadow:0 6px 18px 0 rgba(0,0,0,.06);box-shadow:0 6px 18px 0 rgba(0,0,0,.06);">
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                        <tr>
+                                            <td style="padding:0 35px;">
+                                                <h1 style="color:#1e1e2d; font-weight:500; margin:0;font-size:32px;font-family:'Rubik',sans-serif;">Thank You For Sign Up!</h1>
+                                              <h3>Just One Step More!</h3>
+                                         
+                                                <span
+                                                    style="display:inline-block; vertical-align:middle; margin:29px 0 26px; border-bottom:1px solid #cecece; width:100px;"></span>
+                                                <p style="color:#455056; font-size:15px;line-height:24px; margin:0;">
+                                                   Thanks again for joining us, we are very happy to see that you become a family member from now. Its really appreciatable. Just do one thing to go on.
+                                                </p>
+                                                <a href="javascript:void(0);"
+                                                    style="background:blue;text-decoration:none !important; font-weight:500; margin-top:35px; color:#fff;text-transform:uppercase; font-size:14px;padding:10px 24px;display:inline-block;border-radius:50px;">C O D E : ${v_idd}</a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="height:40px;">&nbsp;</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                            <tr>
+                                <td style="height:20px;">&nbsp;</td>
+                            </tr>
+                            <tr>
+                                <td style="text-align:center;">
+                                   
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="height:80px;">&nbsp;</td>
+                            </tr>
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <!--/100% body table-->
+        </body>
+        
+        </html>
                     `,
       };
   
